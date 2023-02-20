4b8275a8199339b08716c2521e5071d80d68c1a0
Helder Neto <120188176+heldernf@users.noreply.github.com>
2023-02-20 02:02:28 -0300

    serach-bar
    adicionado lupa ao search, melhorado recurso.

--- js/search-script.js
+++ js/search-script.js
@@ -1,4 +1,5 @@
 const searchBar = document.getElementById('search-bar');
+const searchButton = document.getElementById('search-button');
 const productsContainer = document.getElementById('products-container');
 const products = productsContainer.getElementsByClassName('container-product');
 const navLinks = document.querySelectorAll('nav a');
@@ -6,51 +7,73 @@ const navLinks = document.querySelectorAll('nav a');
 let platformFilter = 'all';
 
 navLinks.forEach(function(link) {
-link.addEventListener('click', function(event) {
+  link.addEventListener('click', function(event) {
     event.preventDefault();
     platformFilter = link.getAttribute('data-platform');
     filterProducts();
-});
+  });
 });
 
-searchBar.addEventListener('input', function () {
-filterProducts();
+searchBar.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    filterProducts();
+  }
+});
+
+searchButton.addEventListener('click', function() {
+  filterProducts();
 });
 
 function filterProducts() {
-const term = searchBar.value.toLowerCase();
-
-Array.from(products).forEach(function (product) {
-    const platformEl = product.querySelector('.description-product');
-    const platform = platformEl.textContent.toLowerCase();
-
-    const titleEls = product.querySelectorAll('.div-pm p');
-    const title = Array.from(titleEls).reduce(
-    (text, el) => text + el.textContent.toLowerCase(),
-    ''
-    );
-
-    let showProduct = true;
-    if (platformFilter !== 'all') {
-    if (platform.indexOf(platformFilter) === -1) {
-        showProduct = false;
+    const term = searchBar.value.trim().toLowerCase();
+  
+    if (term === '') {
+      Array.from(products).forEach(function(product, index) {
+        product.style.order = index;
+        product.style.display = 'block';
+      });
+      return;
     }
-    }
-    for (let i = 0; i < term.length; i++) {
-    const char = term.charAt(i);
-    const index = title.indexOf(char);
-    if (index === -1) {
-        showProduct = false;
-        break;
-    } else {
-        title.slice(index + 1);
-    }
-    }
-
-    if (showProduct) {
-    product.style.display = 'block';
-    } else {
-    product.style.display = 'none';
-    }
-});
+  
+    Array.from(products).forEach(function(product) {
+      const titleEls = product.querySelectorAll('.div-pm p');
+      const title = Array.from(titleEls).reduce(
+        (text, el) => text + el.textContent.toLowerCase(),
+        ''
+      );
+  
+      const platformEl = product.querySelector('.description-product');
+      const platform = platformEl.textContent.toLowerCase();
+  
+      let score = 0;
+      let idx = -1;
+      for (let i = 0; i < term.length; i++) {
+        const char = term.charAt(i);
+        const index = title.indexOf(char, idx + 1);
+        if (index === -1) {
+          break;
+        }
+        score += 1 / (index + 1);
+        idx = index;
+      }
+  
+      if (platformFilter !== 'all' && platform.indexOf(platformFilter) === -1) {
+        score = -1;
+      }
+  
+      product.dataset.score = score;
+    });
+  
+    Array.from(products)
+      .sort((a, b) => b.dataset.score - a.dataset.score)
+      .forEach(function(product, index) {
+        product.style.order = index;
+        product.style.display = 'block';
+      });
+  
+    Array.from(products).forEach(function(product) {
+      if (product.dataset.score <= 0) {
+        product.style.display = 'none';
+      }
+    });
 }
