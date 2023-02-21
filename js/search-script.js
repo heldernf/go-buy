b66469f44a93888cb82ff537e8044a90778f75f3
Helder Neto <120188176+heldernf@users.noreply.github.com>
2023-02-20 21:30:04 -0300

    Tentando terminar o algoritimo de pesquisa da search bar

--- js/search-script.js
+++ js/search-script.js
@@ -3,8 +3,9 @@ const searchButton = document.getElementById('search-button');
 const productsContainer = document.getElementById('products-container');
 const products = productsContainer.getElementsByClassName('container-product');
 const navLinks = document.querySelectorAll('nav a');
-
+const loadMoreButton = document.getElementById('load-more');
 let platformFilter = 'all';
+let visibleProducts = 5;
 
 navLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
@@ -24,56 +25,175 @@ searchButton.addEventListener('click', function() {
   filterProducts();
 });
 
+loadMoreButton.addEventListener('click', function() {
+  visibleProducts += 5;
+  showProducts();
+});
+
 function filterProducts() {
-    const term = searchBar.value.trim().toLowerCase();
-  
-    if (term === '') {
-      Array.from(products).forEach(function(product, index) {
-        product.style.order = index;
-        product.style.display = 'block';
-      });
-      return;
+  const term = searchBar.value.trim().toLowerCase();
+
+  if (term === '') {
+    Array.from(products).forEach(function(product, index) {
+      product.style.order = index;
+      product.style.display = 'block';
+    });
+    visibleProducts = 5;
+    showProducts();
+    return;
+  }
+
+  Array.from(products).forEach(function(product) {
+    const titleEls = product.querySelectorAll('.div-pm p');
+    const title = Array.from(titleEls).reduce(
+      (text, el) => text + el.textContent.toLowerCase(),
+      ''
+    );
+
+    const platformEl = product.querySelector('.description-product');
+    const platform = platformEl.textContent.toLowerCase();
+
+    let score = 0;
+    let idx = -1;
+    for (let i = 0; i < term.length; i++) {
+      const char = term.charAt(i);
+      const index = title.indexOf(char, idx + 1);
+      if (index === -1) {
+        break;
+      }
+      score += 1 / (index + 1);
+      idx = index;
     }
-  
-    Array.from(products).forEach(function(product) {
-      const titleEls = product.querySelectorAll('.div-pm p');
-      const title = Array.from(titleEls).reduce(
-        (text, el) => text + el.textContent.toLowerCase(),
-        ''
-      );
-  
-      const platformEl = product.querySelector('.description-product');
-      const platform = platformEl.textContent.toLowerCase();
-  
-      let score = 0;
-      let idx = -1;
-      for (let i = 0; i < term.length; i++) {
-        const char = term.charAt(i);
-        const index = title.indexOf(char, idx + 1);
-        if (index === -1) {
-          break;
-        }
-        score += 1 / (index + 1);
-        idx = index;
-      }
-  
-      if (platformFilter !== 'all' && platform.indexOf(platformFilter) === -1) {
-        score = -1;
-      }
-  
-      product.dataset.score = score;
+
+    if (platformFilter !== 'all' && platform.indexOf(platformFilter) === -1) {
+      score = -1;
+    }
+
+    product.dataset.score = score;
+  });
+
+  Array.from(products)
+    .sort((a, b) => b.dataset.score - a.dataset.score)
+    .forEach(function(product, index) {
+      product.style.order = index;
+      product.style.display = 'block';
     });
-  
-    Array.from(products)
-      .sort((a, b) => b.dataset.score - a.dataset.score)
-      .forEach(function(product, index) {
-        product.style.order = index;
-        product.style.display = 'block';
-      });
-  
-    Array.from(products).forEach(function(product) {
-      if (product.dataset.score <= 0) {
-        product.style.display = 'none';
-      }
-    });
+
+  Array.from(products).forEach(function(product) {
+    if (product.dataset.score <= 0) {
+      product.style.display = 'none';
+    }
+  });
+
+  if (term !== '') {
+    visibleProducts = 5;
+  }
+
+  showProducts();
 }
+
+function showProducts() {
+  for (let i = 0; i < products.length; i++) {
+    if (i < visibleProducts) {
+      products[i].style.display = 'block';
+    } else {
+      products[i].style.display = 'none';
+    }
+  }
+
+  if (visibleProducts >= products.length) {
+    loadMoreButton.style.display = 'none';
+  } else {
+    loadMoreButton.style.display = 'block';
+  }
+}
+
+showProducts();
+k
+
+
+
+
+
+
+
+
+// const searchBar = document.getElementById('search-bar');
+// const searchButton = document.getElementById('search-button');
+// const productsContainer = document.getElementById('products-container');
+// const products = productsContainer.getElementsByClassName('container-product');
+// const navLinks = document.querySelectorAll('nav a');
+
+// let platformFilter = 'all';
+
+// navLinks.forEach(function(link) {
+//   link.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     platformFilter = link.getAttribute('data-platform');
+//     filterProducts();
+//   });
+// });
+
+// searchBar.addEventListener('keyup', function(event) {
+//   if (event.keyCode === 13) {
+//     filterProducts();
+//   }
+// });
+
+// searchButton.addEventListener('click', function() {
+//   filterProducts();
+// });
+
+// function filterProducts() {
+//     const term = searchBar.value.trim().toLowerCase();
+  
+//     if (term === '') {
+//       Array.from(products).forEach(function(product, index) {
+//         product.style.order = index;
+//         product.style.display = 'block';
+//       });
+//       return;
+//     }
+  
+//     Array.from(products).forEach(function(product) {
+//       const titleEls = product.querySelectorAll('.div-pm p');
+//       const title = Array.from(titleEls).reduce(
+//         (text, el) => text + el.textContent.toLowerCase(),
+//         ''
+//       );
+  
+//       const platformEl = product.querySelector('.description-product');
+//       const platform = platformEl.textContent.toLowerCase();
+  
+//       let score = 0;
+//       let idx = -1;
+//       for (let i = 0; i < term.length; i++) {
+//         const char = term.charAt(i);
+//         const index = title.indexOf(char, idx + 1);
+//         if (index === -1) {
+//           break;
+//         }
+//         score += 1 / (index + 1);
+//         idx = index;
+//       }
+  
+//       if (platformFilter !== 'all' && platform.indexOf(platformFilter) === -1) {
+//         score = -1;
+//       }
+  
+//       product.dataset.score = score;
+//     });
+  
+//     Array.from(products)
+//       .sort((a, b) => b.dataset.score - a.dataset.score)
+//       .forEach(function(product, index) {
+//         product.style.order = index;
+//         product.style.display = 'block';
+//       });
+  
+//     Array.from(products).forEach(function(product) {
+//       if (product.dataset.score <= 0) {
+//         product.style.display = 'none';
+//       }
+//     });
+// }
