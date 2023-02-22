ce8e3385a09beb686678b8453457eb0c64844f70
Helder Neto <120188176+heldernf@users.noreply.github.com>
2023-02-22 17:59:51 -0300

    Update search-script.js

--- js/search-script.js
+++ js/search-script.js
@@ -4,46 +4,40 @@ const productsContainer = document.getElementById('products-container');
 const products = productsContainer.getElementsByClassName('container-product');
 const navLinks = document.querySelectorAll('nav a');
 const loadMoreButton = document.getElementById('load-more');
+
 let platformFilter = 'all';
-let visibleProducts = 5;
+let visibleProducts = 10;
 
 navLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
     platformFilter = link.getAttribute('data-platform');
+    visibleProducts = 10;
     filterProducts();
   });
 });
 
 searchBar.addEventListener('keyup', function(event) {
   if (event.keyCode === 13) {
+    visibleProducts = 10;
     filterProducts();
   }
 });
 
 searchButton.addEventListener('click', function() {
+  visibleProducts = 10;
   filterProducts();
 });
 
 loadMoreButton.addEventListener('click', function() {
-  visibleProducts += 5;
-  showProducts();
+  visibleProducts += 10;
+  filterProducts();
 });
 
 function filterProducts() {
   const term = searchBar.value.trim().toLowerCase();
 
-  if (term === '') {
-    Array.from(products).forEach(function(product, index) {
-      product.style.order = index;
-      product.style.display = 'block';
-    });
-    visibleProducts = 5;
-    showProducts();
-    return;
-  }
-
-  Array.from(products).forEach(function(product) {
+  Array.from(products).forEach(function(product, index) {
     const titleEls = product.querySelectorAll('.div-pm p');
     const title = Array.from(titleEls).reduce(
       (text, el) => text + el.textContent.toLowerCase(),
@@ -70,46 +64,28 @@ function filterProducts() {
     }
 
     product.dataset.score = score;
+    product.style.display = 'none';
   });
 
   Array.from(products)
     .sort((a, b) => b.dataset.score - a.dataset.score)
     .forEach(function(product, index) {
-      product.style.order = index;
-      product.style.display = 'block';
+      if (index < visibleProducts) {
+        product.style.order = index;
+        product.style.display = 'block';
+      }
     });
 
-  Array.from(products).forEach(function(product) {
-    if (product.dataset.score <= 0) {
-      product.style.display = 'none';
-    }
-  });
-
-  if (term !== '') {
-    visibleProducts = 5;
-  }
-
-  showProducts();
-}
-
-function showProducts() {
-  for (let i = 0; i < products.length; i++) {
-    if (i < visibleProducts) {
-      products[i].style.display = 'block';
-    } else {
-      products[i].style.display = 'none';
-    }
-  }
-
-  if (visibleProducts >= products.length) {
+  if (visibleProducts >= Array.from(products).filter(product => product.style.display !== 'none').length) {
     loadMoreButton.style.display = 'none';
   } else {
     loadMoreButton.style.display = 'block';
   }
 }
 
-showProducts();
-k
+
+
+
 
 
 
@@ -123,8 +99,9 @@ k
 // const productsContainer = document.getElementById('products-container');
 // const products = productsContainer.getElementsByClassName('container-product');
 // const navLinks = document.querySelectorAll('nav a');
-
+// const loadMoreButton = document.getElementById('load-more');
 // let platformFilter = 'all';
+// let visibleProducts = 5;
 
 // navLinks.forEach(function(link) {
 //   link.addEventListener('click', function(event) {
@@ -144,56 +121,87 @@ k
 //   filterProducts();
 // });
 
+// loadMoreButton.addEventListener('click', function() {
+//   visibleProducts += 5;
+//   showProducts();
+// });
+
 // function filterProducts() {
-//     const term = searchBar.value.trim().toLowerCase();
-  
-//     if (term === '') {
-//       Array.from(products).forEach(function(product, index) {
-//         product.style.order = index;
-//         product.style.display = 'block';
-//       });
-//       return;
+//   const term = searchBar.value.trim().toLowerCase();
+
+//   if (term === '') {
+//     Array.from(products).forEach(function(product, index) {
+//       product.style.order = index;
+//       product.style.display = 'block';
+//     });
+//     visibleProducts = 5;
+//     showProducts();
+//     return;
+//   }
+
+//   Array.from(products).forEach(function(product) {
+//     const titleEls = product.querySelectorAll('.div-pm p');
+//     const title = Array.from(titleEls).reduce(
+//       (text, el) => text + el.textContent.toLowerCase(),
+//       ''
+//     );
+
+//     const platformEl = product.querySelector('.description-product');
+//     const platform = platformEl.textContent.toLowerCase();
+
+//     let score = 0;
+//     let idx = -1;
+//     for (let i = 0; i < term.length; i++) {
+//       const char = term.charAt(i);
+//       const index = title.indexOf(char, idx + 1);
+//       if (index === -1) {
+//         break;
+//       }
+//       score += 1 / (index + 1);
+//       idx = index;
 //     }
-  
-//     Array.from(products).forEach(function(product) {
-//       const titleEls = product.querySelectorAll('.div-pm p');
-//       const title = Array.from(titleEls).reduce(
-//         (text, el) => text + el.textContent.toLowerCase(),
-//         ''
-//       );
-  
-//       const platformEl = product.querySelector('.description-product');
-//       const platform = platformEl.textContent.toLowerCase();
-  
-//       let score = 0;
-//       let idx = -1;
-//       for (let i = 0; i < term.length; i++) {
-//         const char = term.charAt(i);
-//         const index = title.indexOf(char, idx + 1);
-//         if (index === -1) {
-//           break;
-//         }
-//         score += 1 / (index + 1);
-//         idx = index;
-//       }
-  
-//       if (platformFilter !== 'all' && platform.indexOf(platformFilter) === -1) {
-//         score = -1;
-//       }
-  
-//       product.dataset.score = score;
+
+//     if (platformFilter !== 'all' && platform.indexOf(platformFilter) === -1) {
+//       score = -1;
+//     }
+
+//     product.dataset.score = score;
+//   });
+
+//   Array.from(products)
+//     .sort((a, b) => b.dataset.score - a.dataset.score)
+//     .forEach(function(product, index) {
+//       product.style.order = index;
+//       product.style.display = 'block';
 //     });
-  
-//     Array.from(products)
-//       .sort((a, b) => b.dataset.score - a.dataset.score)
-//       .forEach(function(product, index) {
-//         product.style.order = index;
-//         product.style.display = 'block';
-//       });
-  
-//     Array.from(products).forEach(function(product) {
-//       if (product.dataset.score <= 0) {
-//         product.style.display = 'none';
-//       }
-//     });
+
+//   Array.from(products).forEach(function(product) {
+//     if (product.dataset.score <= 0) {
+//       product.style.display = 'none';
+//     }
+//   });
+
+//   if (term !== '') {
+//     visibleProducts = 5;
+//   }
+
+//   showProducts();
 // }
+
+// function showProducts() {
+//   for (let i = 0; i < products.length; i++) {
+//     if (i < visibleProducts) {
+//       products[i].style.display = 'block';
+//     } else {
+//       products[i].style.display = 'none';
+//     }
+//   }
+
+//   if (visibleProducts >= products.length) {
+//     loadMoreButton.style.display = 'none';
+//   } else {
+//     loadMoreButton.style.display = 'block';
+//   }
+// }
+
+// showProducts();
